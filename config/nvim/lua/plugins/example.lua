3ac39059d8179b78d8f336c59386397a8bcff953
Ruslan Gonzalez <ruslan.gonzalez@rise-world.com>
2024-06-01 11:03:19 +0200

    update nvim settings

--- config/nvim/lua/plugins/example.lua
+++ config/nvim/lua/plugins/example.lua
@@ -1,6 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
--- if true then return {} end
+if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -161,25 +161,25 @@ return {
     end,
   },
 
-  -- the opts function can also be used to change the default opts:
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, "😄")
-    end,
-  },
+ -- the opts function can also be used to change the default opts:
+ -- {
+ --   "nvim-lualine/lualine.nvim",
+ --   event = "VeryLazy",
+ --   opts = function(_, opts)
+ --     table.insert(opts.sections.lualine_x, "😄")
+ --   end,
+ -- },
 
   -- or you can return new options to override all the defaults
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function()
-      return {
-        --[[add your custom lualine config here]]
-      }
-    end,
-  },
+  --{
+  --  "nvim-lualine/lualine.nvim",
+  --  event = "VeryLazy",
+  --  opts = function()
+  --    return {
+  --      --[[add your custom lualine config here]]
+  --    }
+  --  end,
+  --},
 
   -- use mini.starter instead of alpha
   { import = "lazyvim.plugins.extras.ui.mini-starter" },
