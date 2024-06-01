bc5b36dfd06b361ec235d6754ee736fa9fcca1fd
Ruslan Gonzalez <ruslan.gonzalez@rise-world.com>
2024-06-01 11:03:51 +0200

    remove alacritty unsuppported setting

--- nixos/packages/alacritty.nix
+++ nixos/packages/alacritty.nix
@@ -7,7 +7,6 @@
       env.TERM = "xterm-256color";
       font = {
         size = 14;
-        draw_bold_text_with_bright_colors = true;
       };
       scrolling.multiplier = 5;
       selection.save_to_clipboard = true;
