e12a38b0df562da3c5a06b0e2a9507a6732f3a41
Ruslan Gonzalez <ruslanguns@gmail.com>
2024-09-04 10:18:03 +0200

    Add additional deps

--- manjaro/setup.sh
+++ manjaro/setup.sh
@@ -10,127 +10,138 @@ sudo pacman -S --noconfirm --needed base-devel fakeroot
 
 # Lista de paquetes a instalar desde los repositorios oficiales
 PACKAGES=(
-    ffmpeg
-    zip
-    neofetch
-    xz
-    unzip
-    p7zip
-    jq
-    yq
-    wget
-    curl
-    fd
-    fzf
-    bat
-    ripgrep
-    nvim
-    tree
-    zoxide
-    zellij
-    mtr
-    iperf3
-    bind-tools
-    ldns
-    aria2
-    socat
-    nmap
-    ipcalc
-    networkmanager-openvpn
-    openfortivpn
-    openvpn
-    glib2
-    gcc
-    grc
-    cowsay
-    file
-    which
-    sed
-    tar
-    gawk
-    zstd
-    gnupg
-    make
-    httpie
-    lazygit
-    tmux
-    xclip
-    pass
-    libreoffice-fresh
-    kubectl
-    docker-compose
-    go
-    rust
-    btop
-    iotop
-    iftop
-    strace
-    ltrace
-    lsof
-    gdu
-    sysstat
-    lm_sensors
-    ethtool
-    pciutils
-    usbutils
-    psensor
-    speedtest-cli
-    fish  # Añadiendo Fish
+  ffmpeg
+  zip
+  neofetch
+  xz
+  unzip
+  p7zip
+  jq
+  yq
+  wget
+  curl
+  fd
+  fzf
+  bat
+  ripgrep
+  neovim
+  tree
+  zoxide
+  zellij
+  mtr
+  iperf3
+  bind-tools
+  ldns
+  aria2
+  socat
+  nmap
+  ipcalc
+  networkmanager-openvpn
+  openfortivpn
+  openvpn
+  glib2
+  gcc
+  grc
+  cowsay
+  file
+  which
+  sed
+  tar
+  gawk
+  zstd
+  gnupg
+  make
+  httpie
+  lazygit
+  tmux
+  xclip
+  pass
+  libreoffice-fresh
+  kubectl
+  docker-compose
+  go
+  rust
+  btop
+  iotop
+  iftop
+  strace
+  ltrace
+  lsof
+  gdu
+  sysstat
+  lm_sensors
+  ethtool
+  pciutils
+  usbutils
+  psensor
+  speedtest-cli
+  fish
+  fisher
+  php
+  composer
+  ruby
+  julia
+  docker
+  flameshot
+  sshpass
+  kubeseal
 )
 
 # Instalar los paquetes listados
 echo "Instalando paquetes..."
 for PACKAGE in "${PACKAGES[@]}"; do
-    sudo pacman -S --noconfirm --needed $PACKAGE
+  sudo pacman -S --noconfirm --needed $PACKAGE
 done
 
 # Instalar yay para manejar AUR
-if ! command -v yay &> /dev/null; then
-    echo "yay no encontrado. Instalando yay..."
-    echo -e "1\n" | sudo pamac install yay --no-confirm --noinput
+if ! command -v yay &>/dev/null; then
+  echo "yay no encontrado. Instalando yay..."
+  echo -e "1\n" | sudo pamac install yay --no-confirm --noinput
 fi
 
 # Lista de paquetes AUR
 AUR_PACKAGES=(
-    eza-git
-    slack-desktop
-    brave-bin
-    element-desktop
-    kind-bin
-    dust-bin
-    visual-studio-code-bin  # VSCode con soporte completo
-    oh-my-fish
+  eza-git
+  slack-desktop
+  brave-bin
+  element-desktop
+  kind-bin
+  dust-bin
+  visual-studio-code-bin
+  oh-my-fish
+  liberica-jdk-17-full-bin
+  fnm
 )
 
 # Instalar paquetes desde AUR
 echo "Instalando paquetes desde AUR..."
 for AUR_PACKAGE in "${AUR_PACKAGES[@]}"; do
-    yay -S --noconfirm $AUR_PACKAGE
+  yay -S --noconfirm $AUR_PACKAGE
 done
 
 # Verificar si Fish ya es el shell por defecto
 CURRENT_SHELL=$(basename "$SHELL")
 if [ "$CURRENT_SHELL" != "fish" ]; then
-    echo "Configurando Fish como shell por defecto..."
-    chsh -s /usr/bin/fish
+  echo "Configurando Fish como shell por defecto..."
+  chsh -s /usr/bin/fish
 else
-    echo "Fish ya está configurado como el shell por defecto."
+  echo "Fish ya está configurado como el shell por defecto."
 fi
 
 # Verificar si oh-my-fish ya está instalado
 if [ ! -d "$HOME/.local/share/omf" ]; then
-    echo "Instalando Oh My Fish..."
-    curl -L https://get.oh-my.fish | fish
+  echo "Instalando Oh My Fish..."
+  curl -L https://get.oh-my.fish | fish
 else
-    echo "Oh My Fish ya está instalado."
+  echo "Oh My Fish ya está instalado."
 fi
 
 # Verificar si Fisher ya está instalado
-if ! fish -c "fisher --version" &> /dev/null; then
-    echo "Instalando Fisher..."
-    fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
+if ! fish -c "fisher --version" &>/dev/null; then
+  echo "Instalando Fisher..."
+  fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 else
-    echo "Fisher ya está instalado."
+  echo "Fisher ya está instalado."
 fi
 
 # Instalar plugins de Fish con Fisher
