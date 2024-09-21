b2768d59afc70d1d1e8d0deb40923c601b7de9f1
Ruslan Gonzalez <ruslanguns@gmail.com>
2024-09-21 16:55:26 +0200

    add supertab and modify copilot behavior

--- config/nvim/lua/plugins/supertab.lua
+++ config/nvim/lua/plugins/supertab.lua
@@ -13,25 +13,36 @@ return {
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
-          -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-          cmp.select_next_item()
+          -- This is commented on purpose,we don't want to select the next item
+          -- cmp.select_next_item()
+          cmp.confirm({ select = true })
         elseif vim.snippet.active({ direction = 1 }) then
-        vim.schedule(function()
+          vim.schedule(function()
             vim.snippet.jump(1)
-        end)
+          end)
         elseif has_words_before() then
           cmp.complete()
         else
           fallback()
         end
       end, { "i", "s" }),
+
       ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
-          cmp.select_prev_item()
+          -- This is commented on purpose,we don't want to select the previous item
+          -- cmp.select_prev_item()
         elseif vim.snippet.active({ direction = -1 }) then
-        vim.schedule(function()
+          vim.schedule(function()
             vim.snippet.jump(-1)
-        end)
+          end)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      ["<CR>"] = cmp.mapping(function(fallback)
+        if cmp.visible() and cmp.get_active_entry() then
+          cmp.confirm({ select = false })
         else
           fallback()
         end
