365481001a9c79b48bc349884689e51da6cbca2d
Ruslan Gonzalez <ruslanguns@gmail.com>
2024-12-25 17:59:08 +0100

    project backups to run At minute 0 past every hour"

--- nix/hosts/nixos/desktop-wsl-01/cron.nix
+++ nix/hosts/nixos/desktop-wsl-01/cron.nix
@@ -2,8 +2,8 @@
   services.cron = {
     enable = true;
     systemCronJobs = [
-      "*/5 * * * * /bin/bash /opt/backup_projects.sh >> /var/log/backup_projects.log 2>&1"
-      "*/5 * * * *      root    date >> /tmp/cron.log"
+      "0 */1 * * *  root  /opt/backup_projects.sh >> /var/log/backup_projects.log 2>&1"
+      "*/5 * * * *  root  date >> /tmp/cron.log"
     ];
   };
 }
