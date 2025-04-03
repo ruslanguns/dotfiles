cb5d209caf7e852556b3c95281849aae88c18500
Ruslan Gonzalez <ruslanguns@gmail.com>
2025-04-03 11:40:49 +0200

    fish: remove completions from terragrunt

--- config/fish/completions/terragrunt.fish
+++
@@ -1,9 +0,0 @@
-
-function __complete_terragrunt
-    set -lx COMP_LINE (commandline -cp)
-    test -z (commandline -ct)
-    and set COMP_LINE "$COMP_LINE "
-    /home/rus/.tenv/Terragrunt/0.76.8/terragrunt
-end
-complete -f -c terragrunt -a "(__complete_terragrunt)"
-
