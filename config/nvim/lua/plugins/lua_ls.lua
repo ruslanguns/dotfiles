b84e19c04fd3ba286579ed8293427d73d074ebfd
Ruslan Gonzalez <ruslanguns@gmail.com>
2025-06-21 14:03:38 +0200

    nvim: add lua ls settings

---
+++ config/nvim/lua/plugins/lua_ls.lua
@@ -0,0 +1,45 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            runtime = { version = "LuaJIT" },
+                            diagnostics = { globals = { "ngx" } },
+                            format = {
+                                enable = false,
+                            },
+                        },
+                    },
+                },
+            },
+            setup = {
+                lua_ls = function(_, opts)
+                    vim.api.nvim_create_autocmd("FileType", {
+                        pattern = "lua",
+                        callback = function()
+                            vim.opt_local.shiftwidth = 4
+                            vim.opt_local.tabstop = 4
+                            vim.opt_local.softtabstop = 4
+                            vim.opt_local.expandtab = true
+                        end,
+                    })
+                    require("lspconfig").lua_ls.setup(opts)
+                    return true
+                end,
+            },
+        },
+    },
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters = {
+                stylua = {
+                    prepend_args = { "--indent-type", "Spaces", "--indent-width", "4" },
+                },
+            },
+        },
+    },
+}
