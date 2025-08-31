c5c0d220a25679b4f7523127bcaec4da0b08799f
Ruslan Gonzalez <ruslanguns@gmail.com>
2025-08-31 10:07:15 +0200

    nvim: fix theme plugin

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -1,14 +1,24 @@
 return {
-  "folke/tokyonight.nvim",
-  opts = {
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
+  { "shaunsingh/nord.nvim" },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      on_highlights = function(hl, c)
+        hl.FloatBorder = { fg = c.blue, bg = "none" }
+        hl.WinSeparator = { fg = c.lightblue } -- border color for vertical split
+      end,
     },
-    on_highlights = function(hl, c)
-      hl.FloatBorder = { fg = c.blue, bg = "none" }
-      hl.WinSeparator = { fg = c.lightblue } -- border color for vertical split
-    end,
+  },
+  -- The LazyVim plugin configures your default colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
   },
 }
